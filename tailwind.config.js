38eb60b3004e0962f804a8d5de2890afe9c2eedb
Angela Tsai <64194102+angelatsai1214@users.noreply.github.com>
2023-05-08 22:16:32 -0700

    Tailwindcss config and Eventify logo

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./client/src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    color: {
+      black: '#000',
+      white: '#fff',
+      teal: '#1CDDDB',
+      sky_blue: '#3EB5E2',
+      purple_blue: '#5A82E8',
+      light_gray: '#D9D9D9',
+      dark_gray: '#464646'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
