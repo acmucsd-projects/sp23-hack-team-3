962cc4729544570b5e97dc0738f060d9f2e86acb
Rachel Chau <rchau@ucsd.edu>
2023-05-16 16:12:47 -0700

    filled in controller functions

--- server/controllers/organizationsController.js
+++ server/controllers/organizationsController.js
@@ -11,11 +11,10 @@ const getOrganizations = async (req, res) => {
 
 //get a single org
 const getOrganization = async (req, res) => {
-    const {id} = req.params
+    const {id} = req.params;
     const organization = await Organization.findById(id)
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({
-        })
+        return res.status(404).json({})
     }
     if (!user) {
         return res.status(404).json({
@@ -26,15 +25,48 @@ const getOrganization = async (req, res) => {
 }
 
 const createOrganization = async (req, res) => {
-
+    const newOrg = req.body;
+    try {
+        await Organization.create(newOrg);
+    }
+    catch (error) {
+        res.status(404).json({message: "error"});
+    }
+    res.status(200).json({message: "Organization created"});
+    // const {organization} = req.body;
+    // const {admin, orgName, socials, events} = organization;
+    // if (!admin || !orgName || !socials || !events) {
+    //     res.status(400).json({error: 'Invalid Input!'});
+    // }
+    // else {
+    //     const newOrganization = await Organization.create(organization);
+    //     res.status(200).json({newOrganization});
+    // }
 }
 
 const deleteOrganizaiton = async (req, res) => {
-
+    const {id} = req.params;
+    try {
+        await Organization.findByIdAndRemove(id)
+        res.status(200).json({message: `Deleted organization with id: ${eventId}`});
+    }
+    catch(error)
+    {
+        res.status(400).json({});
+    }
 }
 
 const updateOrganization = async (req, res) => {
-
+    const {id} = req.params;
+    const updatedInfo = req.body;
+    try 
+    {
+        await Event.findOneAndUpdate({ "_id": id }, { "$set": updatedInfo});  
+    }
+    catch (error) {
+        res.status(404).json({});
+    }
+    res.status(200).json({message: "Update successful"});
 }
 
 module.exports = {getOrganizations, getOrganization, createOrganization, deleteOrganizaiton, updateOrganization};
--- server/models/organizations.js
+++ server/models/organizations.js
@@ -4,10 +4,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const OrgSchema = new Schema({
-    id: {
-        type: Number,
-        required: true
-    },
     admin: {
         type: [Schema.Types.ObjectID],
         ref: 'User',
