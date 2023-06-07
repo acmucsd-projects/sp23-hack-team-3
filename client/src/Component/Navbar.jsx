1c9eccd03b1848949e246d3c805637e909b9e684
Angela Tsai <64194102+angelatsai1214@users.noreply.github.com>
2023-06-07 11:43:11 -0700

    handleLogout

--- client/src/Component/Navbar.jsx
+++ client/src/Component/Navbar.jsx
@@ -1,5 +1,17 @@
 export default function Navbar({ loggedIn }){
     console.log("In navbar: ", loggedIn)
+    
+    const handleLogout = () => {
+        axios.get('http://localhost:4000/users/logout')
+        .then(response => {
+            console.log(response)
+            console.log(response.status)
+        })
+        .catch(err => {
+            console.log("Error logging out: ", err)
+        })
+    }
+    
     return(
        <div style={{ display:'flex', justifyContent: 'space-around', alignItems: 'center', boxShadow: '0px 3px #D9D9D9', position: 'sticky', top: 0, zIndex: 1, backgroundColor: "white", height: '100px' }}>
             <div>
@@ -10,7 +22,7 @@ export default function Navbar({ loggedIn }){
                 <>
                 <div><a href="/post"><button style = {{ cursor: 'pointer', fontWeight: 'bold', borderRadius: 20, border: 'none', padding: 10, paddingRight: 23, paddingLeft: 23, backgroundColor: '#5785E8', color: 'white', width: 150, marginRight: 10 }}>Create an Event</button></a></div>
                 <div><a href="/profile"><button style = {{ cursor: 'pointer', fontWeight: 'bold', borderRadius: 20, border: 'none', padding: 10, paddingRight: 25, paddingLeft: 25, backgroundColor: '#5785E8', color: 'white', width: 150, marginRight: 10 }}>View Profile</button></a></div>
-                <div><a href="/profile"><button style = {{ cursor: 'pointer', fontWeight: 'bold', borderRadius: 20, border: 'none', padding: 10, paddingRight: 25, paddingLeft: 25, backgroundColor: '#5785E8', color: 'white', width: 150 }}>Logout</button></a></div>
+                <div><button onClick={handleLogout} style = {{ cursor: 'pointer', fontWeight: 'bold', borderRadius: 20, border: 'none', padding: 10, paddingRight: 25, paddingLeft: 25, backgroundColor: '#5785E8', color: 'white', width: 150 }}>Logout</button></div>
                 </>
                 }
                 {loggedIn == false &&
