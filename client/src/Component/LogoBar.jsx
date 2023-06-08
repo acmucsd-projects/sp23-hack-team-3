bc6320ad81c5fc9eecdb9c84add3e03b87813803
cjt002@ucsd.edu <64194102+angelatsai1214@users.noreply.github.com>
2023-06-07 23:12:28 -0700

    logobar style and refresh page after logout

--- client/src/Component/LogoBar.jsx
+++ client/src/Component/LogoBar.jsx
@@ -1,6 +1,6 @@
 export default function LogoBar(){
     return(
-        <div style={{ marginRight: 'auto', marginLeft: 'auto', boxShadow: '0px 3px #D9D9D9', textAlign: "center", height: '100px' }}>
+        <div style={{ marginRight: 'auto', marginLeft: 'auto', boxShadow: '0px 3px #1CDDDB', textAlign: "center", height: '100px' }}>
             <a href="/"><img src="../../eventify-logo.png" alt="Logo" style = {{ width: '18%'}} /></a>
         </div> 
     )
